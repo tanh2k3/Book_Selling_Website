6cb2da65ac839648172953a76b3136c5e9e82f41
hanh256 <anhtdh@selex.vn>
2024-10-20 23:10:33 +0700

    update itemList

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import ItemsList from "./itemsList";
+
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h2>Khám phá theo danh mục</h2>
+      <ItemsList />
+    </div>
+  );
 }
+
 
 export default HomePage;
