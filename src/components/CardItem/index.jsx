7a3e1aaeb7fda22b2c956a67b8ec80ea0c662b40
haianh <haianhtranmanh@gmail.com>
2024-10-27 23:29:07 +0700

    merge cardItem

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,33 +1,41 @@
 import React from "react";
 import "./styles.css";
-import {formatPrice} from "../../utils/index.js"
+import { formatPrice } from "../../utils/index.js";
+import { Link } from "react-router-dom";
 
-const CardItem = (item) => {
+const CardItem = ({ item }) => {
   const defaultItem = {
-    imgSrc: "https://cafebiz.cafebizcdn.vn/2019/3/12/photo-1-1552354590822522314238.jpg",
+    id: 0,
+    imgSrc:
+      "https://cafebiz.cafebizcdn.vn/2019/3/12/photo-1-1552354590822522314238.jpg",
     title: "Title",
     description: "Description",
     price: 100000,
     discount: 10,
-    sold:20
+    sold: 20,
   };
 
-  const { imgSrc, title, description, price, discount, sold } = { ...defaultItem, ...item };
+  const { id, imgSrc, title, description, price, discount, sold } = {
+    ...defaultItem,
+    ...item,
+  };
 
   const priceAfterDiscount = (price) => {
     return formatPrice(price - (price * discount) / 100);
-  }
+  };
 
   return (
     <div className="cardItem">
       <div className="imageItem">
         <img src={imgSrc} alt={title} />
       </div>
-      <h3>{title}</h3>
-        <p className="currPrice">{priceAfterDiscount(price)}₫</p>
-        <div className="oldPrice">
-          <p className="discountPercent">{discount}%</p>
-          <strong>{formatPrice(price)}₫</strong>
+      <Link to={`/book/${id}`} className="product-title-link">
+        <h3 className="product-title">{title}</h3>
+      </Link>
+      <p className="currPrice">{priceAfterDiscount(price)}₫</p>
+      <div className="oldPrice">
+        <p className="discountPercent">{discount}%</p>
+        <strong>{formatPrice(price)}₫</strong>
       </div>
       <p className="amount">Đã bán: {sold}</p>
     </div>
--- src/pages/HomePage/productList.jsx
+++ src/pages/HomePage/productList.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './productList.css';
+import CardItem from "../../components/CardItem";
 
 const productList = ({ books }) => {
   return (
     <div className="products-container">
       {books.map((book, index) => (
-        <div key={index} className="product-item">
-          <img src={book.imgSrc} alt={book.title} className="product-image" />
-          <Link to={`/book/${book.id}`} className="product-title-link">
-            <h3 className="product-title">{book.title}</h3>
-          </Link>
-          <p className="product-price">{book.price}</p>
-          <p className="product-discount">{book.discount}</p>
-          <p className="product-sold">{book.sold}</p>
-        </div>
+        <CardItem key={index} item={book} />
       ))}
     </div>
   );
