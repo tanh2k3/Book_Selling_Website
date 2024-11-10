4b28e357eb8ded89a24497a0a78154cc43fb345c
hanh256 <anhtdh@selex.vn>
2024-11-10 16:45:14 +0700

    update item list

--- FrontEnd/src/pages/HomePage/ItemsList/ItemsList.jsx
+++ FrontEnd/src/pages/HomePage/ItemsList/ItemsList.jsx
@@ -1,36 +1,32 @@
-import React from "react";
-import './itemsList.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ItemsList.css';
 
 const items = [
-  {
-    imgSrc: "src/assets/english_book.png",
-    title: "English Books",
-  },
-  {
-    imgSrc: "src/assets/vietnamese_book.png",
-    title: "Sách tiếng Việt",
-  },
-  {
-    imgSrc: "src/assets/van_phong_pham.png",
-    title: "Văn phòng phẩm",
-  },
-  {
-    imgSrc: "src/assets/qua_luu_niem.png",
-    title: "Quà lưu niệm",
-  },
+  { imgSrc: 'src/assets/english_book.png', title: 'English Books' },
+  { imgSrc: 'src/assets/vietnamese_book.png', title: 'Sách tiếng Việt' },
+  { imgSrc: 'src/assets/van_phong_pham.png', title: 'Văn phòng phẩm' },
+  { imgSrc: 'src/assets/qua_luu_niem.png', title: 'Quà lưu niệm' },
 ];
 
-const ItemsList = () => {
-  return (
-    <div className="container">
-      {items.map((item, index) => (
-        <div key={index} className="item">
-          <img src={item.imgSrc} alt={item.title} />
-          <p>{item.title}</p>
-        </div>
-      ))}
-    </div>
-  );
+const ItemsList = () => (
+  <div className="container">
+    {items.map((item, index) => (
+      <div key={index} className="item">
+        <img src={item.imgSrc} alt={item.title} />
+        <p>{item.title}</p>
+      </div>
+    ))}
+  </div>
+);
+
+ItemsList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      imgSrc: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default ItemsList;
