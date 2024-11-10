420c6bf22369a51675573547acab970a0a60ebbc
hanh256 <anhtdh@selex.vn>
2024-11-10 17:56:28 +0700

    update css for display product

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 
@@ -42,13 +41,32 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Endpoint to display the product list
 app.get('/api/products', async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
     console.error('Error fetching products:', error);
+    res.status(500).json({ status: 'error', message: 'Server error' });
+  }
+});
+
+app.get('/api/books/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: 'Invalid book ID format' });
+  }
+
+  try {
+    const book = await Product.findById(id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: 'Book not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching book:', error);
     res.status(500).json({ status: 'error', message: 'Server error' });
   }
 });
--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   imgSrc: { type: String, required: true },
   title: { type: String, required: true },
+  author: { type: String, required: true },
   price: { type: String, required: true },
+  originalPrice: { type: String },
   discount: { type: String },
-  sold: { type: String },
+  rating: { type: Number },
+  reviewsCount: { type: Number },
+  soldCount: { type: String },
+  features: [{ type: String }],
+  similarBooks: [{
+    title: { type: String },
+    imgSrc: { type: String }
+  }]
 });
 
 module.exports = mongoose.model('Product', productSchema);
