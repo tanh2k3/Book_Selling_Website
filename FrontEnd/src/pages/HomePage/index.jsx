bd5fea57df0fed20c90af4969de36e484c5a3644
Tuan Anh <trantuananh.bo2093@gmail.com>
2024-11-23 17:41:25 +0700

    update 2311

--- FrontEnd/src/pages/HomePage/index.jsx
+++ FrontEnd/src/pages/HomePage/index.jsx
@@ -5,18 +5,31 @@ import TopSellingProduct from "./TopSellingProduct";
 import ClassicBrand from "./ClassicBrand";
 import VoucherList from "./Voucher/Voucher";
 import FlashSale from "./FlashSale/FlashSale";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Profile from "../Profile";
 
 const HomePage = () => {
   return (
     <div>
-      <TopSellingProduct />
-      <h2>Khám phá theo danh mục</h2>
-      <ClassicBrand />
-      <FlashSale />
-      <VoucherList numberOfVouchers={10} />
-      <h2>Danh sách sản phẩm</h2>
-      <ItemsList />
-      <ProductsList />
+      <Header />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <TopSellingProduct />
+            <h2>Khám phá theo danh mục</h2>
+            <ClassicBrand />
+            <FlashSale />
+            <VoucherList numberOfVouchers={10} />
+            <h2>Danh sách sản phẩm</h2>
+            <ItemsList />
+            <ProductsList />
+          </>} 
+        />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
